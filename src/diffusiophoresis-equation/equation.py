d5a593d7b6fe3ddbe845e5bc3a04fb776eb46fd8
MFreit50 <michaelfreitas.mf50@gmail.com>
2024-09-26 20:47:05 -0400

    Moved Folders to src to facilitate python imports

--- src/diffusiophoresis-equation/equation.py
+++ src/diffusiophoresis-equation/equation.py
@@ -1,8 +1,11 @@
 from equation_formulas import Equation_Formulas
+from ..simulations.diffusiophoretic_particle_tracker import DiffusiophoreticParticleTracker
+from variable import Variable
+import numpy as np
 class Equation(Equation_Formulas):
     def __init__(self, absolute_temperature, electrophoretic_mobility, chemiphoretic_mobility) -> None:
         super().__init__()
-
+        self.variable_array = np.array()
         self.absolute_temperature = absolute_temperature
 
         #channel variables:
@@ -17,14 +20,23 @@ class Equation(Equation_Formulas):
         self.electrophoretic_mobility = electrophoretic_mobility
         self.chemiphoretic_mobility = chemiphoretic_mobility
 
+        self.particle_tracker : DiffusiophoreticParticleTracker = DiffusiophoreticParticleTracker()
         #set constant flags:
         self.constant_flags = {
             'absolute_temperature': False
         }
     
+    def get_variable(self, name):
+        return
+
+    def set_variable(self, name, value):
+        pass
     
     def get_variable_names_list(self) -> list:
         return 
+    
+    def get_variable_array(self) -> np.array:
+        return
     
     def get_channel_area(self) -> float:
         return self.channel_height * self.channel_width
@@ -37,3 +49,6 @@ class Equation(Equation_Formulas):
     
     def is_laminar(self) -> bool:
         self.reynolds_number(self.density, self.fluid_velocity, self.characteristic_length, self.dynamic_viscosity)
+    
+    def get_exclusion_zone_area(self) -> float:
+        return self.particle_tracker.calculate_exclusion_zone_area()#need to pass parameters
