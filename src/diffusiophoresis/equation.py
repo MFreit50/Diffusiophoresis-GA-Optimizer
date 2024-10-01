3fd090be6712f17a2cca35145a85ecd869e3124d
MFreit50 <michaelfreitas.mf50@gmail.com>
2024-09-30 23:52:14 -0400

    Added modularity to equation calculation
    an equation will not be calculated if its result has been provided

--- src/diffusiophoresis/equation.py
+++ src/diffusiophoresis/equation.py
@@ -34,21 +34,22 @@ class Equation():
     def randomize_equation(self) -> None:
         for _, variable in self.variables.items():
             variable.randomize()
-
-    def channel_area(self) -> float:
-        channel_height = self.get_value("channel_height")
-        channel_length = self.get_value("channel_length")
-        return channel_height * channel_length
     
     #Calculations
     def get_exlcusion_zone_area(self):
+        if self.has_variable("exclusion_zone_area"):
+            return self.get_value("exlcusion_zone_area")
+        
         channel_height = self.get_value("channel_height")
-        channel_length = self.get_value("channel_height")
+        channel_length = self.get_value("channel_length")
         mean_velocity = 1 #need to get this value
         diffusiophoretic_velocity = self.get_diffusiophoretic_velocity()
         return formula.exclusion_zone_area(channel_height, channel_length, mean_velocity, diffusiophoretic_velocity)
     
     def get_diffusiophoretic_velocity(self) -> float:
+        if self.has_variable("diffusiophoretic_velocity"):
+            return self.get_value("diffusiophoretic_velocity")
+        
         beta_potential = self.get_beta_potential()
         electrophoretic_mobility = self.get_electrophoretic_mobility()
         chemiphoretic_mobility = self.get_chemiphoretic_mobility()
@@ -58,12 +59,16 @@ class Equation():
     def get_beta_potential(self):
         if self.has_variable("beta_potential"):
             return self.get_value("beta_potential")
+        
         else:
             cation = self.get_value("cation")
             anion = self.get_value("anion")
             return formula.beta_potential(cation, anion)
 
     def get_electrophoretic_mobility(self) -> float:
+        if self.has_variable("electrophoretic_mobility"):
+            return self.get_value("electrophoretic_mobility")
+        
         valence_charge = self.get_value("valence_charge")
         elementary_charge = self.get_value("elementary_charge")
         zeta_potential = self.get_value("zeta_potential")
@@ -71,6 +76,9 @@ class Equation():
         return formula.electrophoretic_mobility(valence_charge, elementary_charge, zeta_potential, absolute_temperature)
         
     def get_chemiphoretic_mobility(self) -> float:
+        if self.has_variable("chemiphoretic_mobility"):
+            return self.get_value("chemiphoretic_mobility")
+        
         valence_charge = self.get_value("valence_charge")
         elementary_charge = self.get_value("elementary_charge")
         zeta_potential = self.get_value("zeta_potential")
@@ -78,15 +86,36 @@ class Equation():
         return formula.chemiphoretic_mobility(valence_charge, elementary_charge, zeta_potential, absolute_temperature)
     
     def get_chemiphoretic_gradient(self) -> float:
+        if self.has_variable("chemiphoretic_gradient"):
+            return self.get_value("chemiphoretic_gradient")
+        
         ci = self.get_value("ci")
         cb = self.get_value("cb")
         channel_length = self.get_value("length")
         return formula.chemiphoretic_gradient(ci, cb, channel_length)
+
+    def get_channel_area(self) -> float:
+        if self.has_variable("channel_area"):
+            return self.get_value("channel_area")
+        
+        channel_height = self.get_value("channel_height")
+        channel_length = self.get_value("channel_length")
+        return formula.channel_area(channel_height, channel_length)
+    
+    def get_characteristic_length(self) -> float:
+        if self.has_variable("characteristic_length"):
+            return self.get_value("characteristic_length")
+        
+        channel_height = self.get_value("channel_height")
+        channel_length = self.get_value("channel_length")
+
+        return formula.characteristic_length(channel_height, channel_length)
 
     def is_laminar(self) -> bool:
         fluid_density = self.get_value("fluid_density")
         fluid_velocity = self.get_value("fluid_velocity")
         characteristic_length = self.get_characteristic_length()
         dynamic_viscosity = self.get_value("dynamic_viscosity")
+        
         reynolds_number = formula.reynolds_number(fluid_density, fluid_velocity, characteristic_length, dynamic_viscosity)
         return reynolds_number < 500
