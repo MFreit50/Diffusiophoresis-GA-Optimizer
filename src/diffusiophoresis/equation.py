3bc3cd82bf343019ba2e7ba5acd8f3fe28428fe8
MFreit50 <michaelfreitas.mf50@gmail.com>
2024-09-30 21:26:07 -0400

    Remade Equation Class for modularity
    new equation class doesnt inherit a parent scene and instead uses a static equation class to save on memory
    
    new equation class also uses a dictionary to keep track of which variables it has access too and wont perform calculations if they were not provided

--- src/diffusiophoresis/equation.py
+++ src/diffusiophoresis/equation.py
@@ -1,54 +1,89 @@
-from equation_formulas import Equation_Formulas
-from ..simulations.diffusiophoretic_particle_tracker import DiffusiophoreticParticleTracker
-from variable import Variable
-import numpy as np
-class Equation(Equation_Formulas):
-    def __init__(self, absolute_temperature, electrophoretic_mobility, chemiphoretic_mobility) -> None:
+from diffusiophoresis.variable import Variable
+from diffusiophoresis.diffusiophoresis_formulas import DiffusiophoresisFormulas as formula
+
+class Equation():
+    def __init__(self) -> None:
         super().__init__()
-        self.variable_array = np.array()
-        self.absolute_temperature = absolute_temperature
+        self.variables = {}
 
-        #channel variables:
-        self.channel_height
-        self.channel_width
-
-        #fluid variables:
-        self.fluid_density
-        self.fluid_viscosity
-        self.fluid_velocity
-
-        self.electrophoretic_mobility = electrophoretic_mobility
-        self.chemiphoretic_mobility = chemiphoretic_mobility
-
-        self.particle_tracker : DiffusiophoreticParticleTracker = DiffusiophoreticParticleTracker()
-        #set constant flags:
-        self.constant_flags = {
-            'absolute_temperature': False
-        }
+    def has_variable(self, name : str) -> bool:
+        return name in self.variables
     
-    def get_variable(self, name):
-        return
+    def add_variable(self, variable : Variable):
+        self.variables[variable.get_name()] = variable
 
-    def set_variable(self, name, value):
-        pass
+    def get_variable(self, name):
+        if name in self.variables:
+            return self.variables[name]
+        raise KeyError(f"Variable '{name}' not found in the equation")
+    
+    def get_value(self, name):
+        return self.get_variable(name).value
+    
+    def set_value(self, name, value):
+        #raises error if variable is constant
+        variable = self.get_variable(name)
+        variable.set_value(value)
     
     def get_variable_names_list(self) -> list:
-        return 
+        return list(self.variables)
+
+    def randomize_equation(self) -> None:
+        for _, variable in self.variables.items():
+            variable.randomize()
+
+    def channel_area(self) -> float:
+        channel_height = self.get_value("channel_height")
+        channel_length = self.get_value("channel_length")
+        return channel_height * channel_length
     
-    def get_variable_array(self) -> np.array:
-        return
+    #Calculations
+    def get_exlcusion_zone_area(self):
+        channel_height = self.get_value("channel_height")
+        channel_length = self.get_value("channel_height")
+        mean_velocity = 1 #need to get this value
+        diffusiophoretic_velocity = self.get_diffusiophoretic_velocity()
+        return formula.exclusion_zone_area(channel_height, channel_length, mean_velocity, diffusiophoretic_velocity)
     
-    def get_channel_area(self) -> float:
-        return self.channel_height * self.channel_width
+    def get_diffusiophoretic_velocity(self) -> float:
+        beta_potential = self.get_beta_potential()
+        electrophoretic_mobility = self.get_electrophoretic_mobility()
+        chemiphoretic_mobility = self.get_chemiphoretic_mobility()
+        chemiphoretic_gradient = self.get_chemiphoretic_gradient()
+        return formula.diffusiophoretic_velocity(beta_potential, electrophoretic_mobility, chemiphoretic_mobility, chemiphoretic_gradient)
+
+    def get_beta_potential(self):
+        if self.has_variable("beta_potential"):
+            return self.get_value("beta_potential")
+        else:
+            cation = self.get_value("cation")
+            anion = self.get_value("anion")
+            return formula.beta_potential(cation, anion)
+
+    def get_electrophoretic_mobility(self) -> float:
+        valence_charge = self.get_value("valence_charge")
+        elementary_charge = self.get_value("elementary_charge")
+        zeta_potential = self.get_value("zeta_potential")
+        absolute_temperature = self.get_value("absolute_temperature")
+        return formula.electrophoretic_mobility(valence_charge, elementary_charge, zeta_potential, absolute_temperature)
+        
+    def get_chemiphoretic_mobility(self) -> float:
+        valence_charge = self.get_value("valence_charge")
+        elementary_charge = self.get_value("elementary_charge")
+        zeta_potential = self.get_value("zeta_potential")
+        absolute_temperature = self.get_value("absolute_temperature")
+        return formula.chemiphoretic_mobility(valence_charge, elementary_charge, zeta_potential, absolute_temperature)
     
-    def set_calculated_electrophoretic_mobility(self, valence_charge, elementary_charge, zeta_potential, cation, anion):
-        return 2 * self.beta_potential(cation, anion) * self.electrophoretic_mobility(valence_charge, elementary_charge, zeta_potential)
-    
-    def set_calculated_chemiphoretic_mobility(self, valence_charge, elementary_charge, zeta_potential):
-        return self.chemiphoretic_mobility(valence_charge, elementary_charge, zeta_potential)
-    
+    def get_chemiphoretic_gradient(self) -> float:
+        ci = self.get_value("ci")
+        cb = self.get_value("cb")
+        channel_length = self.get_value("length")
+        return formula.chemiphoretic_gradient(ci, cb, channel_length)
+
     def is_laminar(self) -> bool:
-        self.reynolds_number(self.density, self.fluid_velocity, self.characteristic_length, self.dynamic_viscosity)
-    
-    def get_exclusion_zone_area(self) -> float:
-        return self.particle_tracker.calculate_exclusion_zone_area()#need to pass parameters
+        fluid_density = self.get_value("fluid_density")
+        fluid_velocity = self.get_value("fluid_velocity")
+        characteristic_length = self.get_characteristic_length()
+        dynamic_viscosity = self.get_value("dynamic_viscosity")
+        reynolds_number = formula.reynolds_number(fluid_density, fluid_velocity, characteristic_length, dynamic_viscosity)
+        return reynolds_number < 500
