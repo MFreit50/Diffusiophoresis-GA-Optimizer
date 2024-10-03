bac3e12167c577345a5b9c6fcc41b1484ad85aa1
MFreit50 <michaelfreitas.mf50@gmail.com>
2024-10-03 14:29:44 -0400

    Fixed Hash function
    the hash function only works with one parameter at a time, to fix this I added all parameters to a tuple before hashing

--- src/diffusiophoresis/variable.py
+++ src/diffusiophoresis/variable.py
@@ -116,7 +116,7 @@ class Variable:
         return self.min_range <= value <= self.max_range
     
     def __hash__(self) -> int:
-        return hash(self.variable_name, self.value, self.is_constant, self.min_range, self.max_range)
+        return hash( (self.variable_name, self.value, self.is_constant, self.min_range, self.max_range) )
 
     def __eq__(self, other) -> int:
         if isinstance(other, Variable):
