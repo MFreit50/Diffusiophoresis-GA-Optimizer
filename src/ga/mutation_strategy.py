f96a1f81b2ef73f4eb0134887cc698f0fa802aaa
MFreit50 <michaelfreitas.mf50@gmail.com>
2024-10-28 14:14:46 -0400

    Removed Mutation rate from mutation strategy
    strategy manager was already responsible for using mutation strategy to determine mutation eligibility

--- src/ga/mutation_strategy.py
+++ src/ga/mutation_strategy.py
@@ -6,35 +6,36 @@ from abc import ABC, abstractmethod
 
 class MutationStrategy(ABC):
     @abstractmethod
-    def mutate(self, mutation_rate: float, child: Equation) -> Equation:
+    def mutate(self, child: Equation) -> Equation:
         pass
 
 class RandomizeMutation(MutationStrategy):
-    def mutate(self, mutation_rate: float, child: Equation) -> Equation:
-        if np.random.rand() > mutation_rate:
-            return child
+    def mutate(self, child: Equation) -> Equation:
         return child.randomize_equation()
     
 class StepMutation(MutationStrategy):
-    def mutate(self, mutation_rate: float, child: Equation) -> Equation:
-        if np.random.rand() > mutation_rate:
-            return child
+    def mutate(self, child: Equation) -> Equation:
+        # Get non constant variables from equation
+        variable_list: list[Variable] = child.get_variable_list(filter_constants=True)
         
-        #get non constant variables from equation
-        variable_list = child.get_variable_list(filter_constants=True)
-        
-        #choose a variable randomly
+        # Choose a variable randomly
         chosen_variable: Variable = random.choice(variable_list)
         
-        #add a small positive or negative step to the value
-        current_value = chosen_variable.get_value()
-        #TODO: A step of +-0.001 is too small for variables with larger range
-
-        step_size = random.uniform(-0.001, 0.001)
+        # Get the max and min range of the chosen variable
+        chosen_variable_max: float = chosen_variable.get_max_range()
+        chosen_variable_min: float = chosen_variable.get_min_range()
         
-        #ensure the new value stays within the bounds of max/min range
-        new_value = np.clip(current_value + step_size, chosen_variable.get_min_range(), chosen_variable.get_max_range())
-        chosen_variable.set_value(new_value)
+        # Calculate the step size and value for the mutation
+        step_factor: float = 0.01
+        step_size: float = step_factor * (chosen_variable_max - chosen_variable_min)
+        step_value: float = random.uniform(-step_size, step_size)
 
+        # Ensure the new value stays within the bounds of max/min range
+        new_value: float = np.clip(chosen_variable.get_value() + step_value, 
+                                   chosen_variable_min, 
+                                   chosen_variable_max)
+        
+        chosen_variable.set_value(new_value)
         child.add_variable(chosen_variable)
+        
         return child
