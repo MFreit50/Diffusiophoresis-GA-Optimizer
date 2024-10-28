b5fa12b5b23e116f06ba4d9140e14ecd8a46b1d2
MFreit50 <michaelfreitas.mf50@gmail.com>
2024-10-28 14:17:23 -0400

    Update strategy_manager.py
    Removed crossover and mutation rates from constructor:
    strategy manager is now solely responsible for determining these rates as the ga was not doing anything with them
    
    updated the return for select parents,crossover, and mutate
    now instead of the ga relying on internal logic to create a new population each generation, it can call these methods once and all data will be provided correctly
    
    Co-Authored-By: Benjamin Jon <98132824+bjon1@users.noreply.github.com>

--- src/ga/strategy_manager.py
+++ src/ga/strategy_manager.py
@@ -6,8 +6,8 @@ from ga.selection_strategy import *
     StrategyManager class is responsible for managing the mutation, crossover, and selection strategies.
     It is responsible for picking the strategies to be used in the genetic algorithm.
 '''
-class StrategyManager:
-    def __init__(self, mutation_rate: float, crossover_rate: float):
+class StrategyManager():
+    def __init__(self):
         self.available_mutation_strategies = {
             "randomize": RandomizeMutation(),
             "step": StepMutation()
@@ -21,8 +21,8 @@ class StrategyManager:
             "tournament": TournamentSelection()
         }
         
-        self.mutation_rate: float = mutation_rate
-        self.crossover_rate: float = crossover_rate
+        self.mutation_rate: float = 0.01
+        self.crossover_rate: float = 0.07
 
         self.mutation_strategy: MutationStrategy = None
         self.crossover_strategy: CrossoverStrategy = None
@@ -32,8 +32,8 @@ class StrategyManager:
         
     def update_strategies(self, diversity_score: float, fitness_scores: list, population_size: int):
         
-        if diversity_score < population_size * 0.15:  # Low diversity
-            self.mutation_rate = min(self.mutation_rate * 1.1, 0.5)
+        if diversity_score < population_size * 0.65:  # Low diversity
+            self.mutation_rate = min(self.mutation_rate * 1.1, 0.95)
             pass
         else:
             self.mutation_rate = max(self.mutation_rate * 0.9, 0.01)
@@ -58,18 +58,41 @@ class StrategyManager:
         choice: SelectionStrategy = random.choice(list(self.available_selection_strategies.values()))
         return choice   
         
-    def mutate(self, child: Equation) -> Equation:
-        if np.random.rand() > self.mutation_rate:
-            return child
-        return self.mutation_strategy.mutate(self.mutation_rate, child)
+    def mutate(self, population: Equation) -> Equation:
+        mutated_population = []
 
-    def crossover(self, parent1: Equation, parent2: Equation) -> tuple[Equation, Equation]:
-        if np.random.rand() > self.crossover_rate:
-            return parent1, parent2
-        return self.crossover_strategy.crossover(self.crossover_rate, parent1, parent2)
+        for individual in population:
+            if np.random.rand() > self.mutation_rate:
+                mutated_population.append(individual)
+            else:
+                self.mutation_strategy = self.pick_mutation_strategy()
+                mutated_individual = self.mutation_strategy.mutate(individual)
+                mutated_population.append(mutated_individual)
+
+        return mutated_population
+
+    def crossover(self, selected_parents : list) -> tuple[Equation, Equation]:
+        offspring = []
+
+        for parent1, parent2 in selected_parents:
+            if np.random.rand() > self.crossover_rate:
+                offspring.extend([parent1, parent2])
+            else:
+                self.crossover_strategy = self.pick_crossover_strategy()
+                child1, child2 = self.crossover_strategy.crossover(parent1, parent2)
+                offspring.extend([child1, child2])
+        
+        return offspring
     
     def select_parents(self, population: list[Equation], fitness_scores: list) -> tuple[Equation, Equation]:
-        return self.selection_strategy.select_parents(population, fitness_scores)
+        selected_population = []
+
+        while len(selected_population)*2 < len(population):
+            self.selection_strategy = self.pick_selection_strategy()
+            parent1, parent2 = self.selection_strategy.select_parents(population, fitness_scores)
+            selected_population.append((parent1, parent2))
+
+        return selected_population
     
     def get_mutation_rate(self) -> float:
         return self.mutation_rate
