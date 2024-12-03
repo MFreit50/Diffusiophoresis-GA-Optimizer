e548faf3c397977ddda86bc6932f6b5e4f8a0a9a
MFreit50 <michaelfreitas.mf50@gmail.com>
2024-12-03 17:01:47 -0500

    Fixed Levy Function
    Levy Function was typed incorrectly

--- src/functions/levy.py
+++ src/functions/levy.py
@@ -8,11 +8,11 @@ class Levy(Function):
     def evaluate(self, x, *args):
         x1 = x[0]
         x2 = x[1]
-        w1 = 1 + (x1 + 1) / 4
-        w2 = 1 + (x2 + 1) / 4
+        w1 = 1 + (x1 - 1) / 4
+        w2 = 1 + (x2 - 1) / 4
         term1 = np.sin(np.pi * w1) ** 2
-        term2 = (w2 - 1) ** 2 * (1 + 10 * np.sin(np.pi * w2 + 1) ** 2)
-        term3 = (w2 - 1) ** 2 * (1 + np.sin(2 * math.pi * w2) ** 2)
+        term2 = (w1 - 1) ** 2 * (1 + 10 * np.sin(np.pi * w2) ** 2)
+        term3 = (w2 - 1) ** 2 * (1 + np.sin(2 * np.pi * w2) ** 2)
         return term1 + term2 + term3
 
     def get_parameters(self):
